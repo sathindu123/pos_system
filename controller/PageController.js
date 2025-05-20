0829ebea98c840f065bf935002dd5c022daf7279
sathindu123 <sathindusathsarakumara@gmail.com>
2025-05-21 02:30:11 +0530

    Add_CSS_OrderView

--- controller/PageController.js
+++ controller/PageController.js
@@ -1,34 +1,46 @@
 $("#customer").css("display", "none");
 $("#item").css("display", "none");
 $("#order-section").css("display", "none");
-$("#place-nav-01").css("display", "none");
+$("#order-history-section").css("display", "none");
 
 
+$("#place-nav-01").on('click', function () {
+    $("#home").css("display", "none");
+    $("#customer").css("display", "none");
+    $("#item").css("display", "none");
+    $("#order-section").css("display", "none");
+    $("#order-history-section").css("display", "block");
+})
 
 $("#home-nav").on('click', function() {
     $("#home").css("display", "block");
     $("#customer").css("display", "none");
     $("#item").css("display", "none");
-    $("#place-nav-01").css("display", "none");
+    $("#order-history-section").css("display", "none");
+    $("#order-section").css("display", "none");
 })
 
 $("#customer-nav").on('click', function () {
+    $("#home").css("display", "none");
     $("#customer").css("display", "block");
     $("#item").css("display", "none");
+    $("#order-history-section").css("display", "none");
     $("#order-section").css("display", "none");
-    $("#place-nav-01").css("display", "none");
 })
 
 $("#item-nav").on('click', function () {
+    $("#home").css("display", "none");
     $("#customer").css("display", "none");
     $("#item").css("display", "block");
-    $("#place-nav-01").css("display", "none");
+    $("#order-history-section").css("display", "none");
     $("#order-section").css("display", "none");
+
 })
 
 $("#place-nav").on('click', function () {
+    $("#home").css("display", "none");
     $("#customer").css("display", "none");
     $("#item").css("display", "none");
     $("#order-section").css("display", "block");
-    $("#place-nav-01").css("display", "none");
+    $("#order-history-section").css("display", "none");
 })
