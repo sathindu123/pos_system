6ce5f46bdfee69628c0bc233aa4c2a586fa6f8a7
sathindu123 <sathindusathsarakumara@gmail.com>
2025-05-21 02:20:54 +0530

    Add_PlaceOrder_HistoryPage

--- controller/PageController.js
+++ controller/PageController.js
@@ -1,6 +1,7 @@
 $("#customer").css("display", "none");
 $("#item").css("display", "none");
 $("#order-section").css("display", "none");
+$("#place-nav-01").css("display", "none");
 
 
 
@@ -8,24 +9,26 @@ $("#home-nav").on('click', function() {
     $("#home").css("display", "block");
     $("#customer").css("display", "none");
     $("#item").css("display", "none");
+    $("#place-nav-01").css("display", "none");
 })
 
 $("#customer-nav").on('click', function () {
     $("#customer").css("display", "block");
     $("#item").css("display", "none");
     $("#order-section").css("display", "none");
+    $("#place-nav-01").css("display", "none");
 })
 
 $("#item-nav").on('click', function () {
     $("#customer").css("display", "none");
     $("#item").css("display", "block");
+    $("#place-nav-01").css("display", "none");
     $("#order-section").css("display", "none");
-
-
 })
 
 $("#place-nav").on('click', function () {
     $("#customer").css("display", "none");
     $("#item").css("display", "none");
     $("#order-section").css("display", "block");
+    $("#place-nav-01").css("display", "none");
 })
