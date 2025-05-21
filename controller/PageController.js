3d7fd27947321cb4c651b66f624d9400f450be9e
sathindu123 <sathindusathsarakumara@gmail.com>
2025-05-21 13:43:02 +0530

    CompiteProject

--- controller/PageController.js
+++ controller/PageController.js
@@ -1,27 +1,34 @@
+
 $("#customer").css("display", "none");
 $("#item").css("display", "none");
 $("#order-section").css("display", "none");
 $("#order-history-section").css("display", "none");
 
-
-$("#place-nav-01").on('click', function () {
-    $("#home").css("display", "none");
-    $("#customer").css("display", "none");
-    $("#item").css("display", "none");
-    $("#order-section").css("display", "none");
-    $("#order-history-section").css("display", "block");
+$(`#getSt`).on('click', function(e) {
+    $("#home-section").css("display", "none");
+    $("#customer").css("display", "block");
 })
 
 $("#home-nav").on('click', function() {
-    $("#home").css("display", "block");
+    $("#home-section").css("display", "block");
     $("#customer").css("display", "none");
     $("#item").css("display", "none");
     $("#order-history-section").css("display", "none");
     $("#order-section").css("display", "none");
 })
 
+$("#place-nav-01").on('click', function () {
+    $("#home-section").css("display", "none");
+    $("#customer").css("display", "none");
+    $("#item").css("display", "none");
+    $("#order-section").css("display", "none");
+    $("#order-history-section").css("display", "block");
+})
+
+
+
 $("#customer-nav").on('click', function () {
-    $("#home").css("display", "none");
+    $("#home-section").css("display", "none");
     $("#customer").css("display", "block");
     $("#item").css("display", "none");
     $("#order-history-section").css("display", "none");
@@ -29,7 +36,7 @@ $("#customer-nav").on('click', function () {
 })
 
 $("#item-nav").on('click', function () {
-    $("#home").css("display", "none");
+    $("#home-section").css("display", "none");
     $("#customer").css("display", "none");
     $("#item").css("display", "block");
     $("#order-history-section").css("display", "none");
@@ -38,7 +45,7 @@ $("#item-nav").on('click', function () {
 })
 
 $("#place-nav").on('click', function () {
-    $("#home").css("display", "none");
+    $("#home-section").css("display", "none");
     $("#customer").css("display", "none");
     $("#item").css("display", "none");
     $("#order-section").css("display", "block");
